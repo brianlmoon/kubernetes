c7f05030c103a78dcd1727b8e90bb685c78a544b
Brian Moon <brian@moonspot.net>
2022-11-01 16:07:13 -0500

    add files

--- build/release.sh
+++ build/release.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR/..
 
 git checkout -b $1 && \
 php ./build/build.php --version $1 && \
+git add src/Objects && \
 git commit -m "Kubernetes $1" && \
 git push origin $1 && \
 git checkout main
