f4fe87c51b59d5303ea9517c6146ed1332d4ac3f
Brian Moon <brian@moonspot.net>
2023-12-14 12:43:00 -0600

    Only merge the latest version to main

--- build/release.sh
+++ build/release.sh
@@ -3,9 +3,19 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
-git checkout -b $1 && \
-php ./build/build.php --version $1 && \
+LATEST=`git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/kubernetes/kubernetes.git | awk '{print $2}' | sed s,refs/tags/,, | fgrep -v '-' | fgrep -v '^' | tail -n 1`
+
+VERSION=${1:-$LATEST}
+
+git checkout -b $VERSION && \
+php ./build/build.php --version $VERSION && \
 git add src/Objects && \
-git commit -m "Kubernetes $1" && \
-git checkout main && \
-git merge $1
+git commit -m "Kubernetes $VERSION" && \
+git tag $VERSION
+
+
+if [ "$VERSION" == "$LATEST" ]
+then
+    git checkout main && \
+    git merge $VERSION
+fi
